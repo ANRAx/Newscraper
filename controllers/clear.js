3dbcc31fe3bcee9eb48a41e7fe60677e36ecf646
Sadhguru <ramdassaj@gmail.com>
2019-12-31 10:20:28 -0500

    add require models and export logic to clear.js

---
+++ controllers/clear.js
@@ -0,0 +1,12 @@
+let db = require("../models");
+
+module.exports = {
+    clearDB: function(req, res) {
+        db.Headline.remove({})
+        .then(function() {
+            return db.Note.remove({});
+        }).then(function() {
+            res.json({ ok: true });           
+        });
+    }
+};
