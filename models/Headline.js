4273281a5610cda388b58a864c43aaa5db245aa1
Sadhguru <ramdassaj@gmail.com>
2020-01-03 17:38:44 -0500

    headline logic in models

---
+++ models/Headline.js
@@ -0,0 +1,40 @@
+// HEADLINE MODEL 
+// ========================================
+
+// Require mongoose
+let mongoose = require("mongoose");
+// Create schema class using a mongoose's schema method
+let Schema = mongoose.Schema;
+
+// Create the headlinesSchema with our schema class
+let headlinesSchema = new Schema({
+    // A String (headline) must be entered
+    headline: {
+        type: String,
+        required: true,
+        unique: { index: { unique: true } }
+    },
+    // A String (summary) must be entered
+    summary: {
+        type: String,
+        required: true
+    },
+    // A String (URL) must be entered
+    url: {
+        type: String,
+        required: true
+    },
+    // date is just a string
+    date: {
+        type: Date,
+        default: false
+    },
+    saved: {
+        type: Boolean,
+        default: false
+    }
+});
+
+let Headline = mongoose.model("Headline", headlinesSchema);
+
+module.exports = Headline;
