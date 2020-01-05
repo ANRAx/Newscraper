91b8a2c04853d454af93cd8f80c3f274641fce93
Sadhguru <ramdassaj@gmail.com>
2020-01-05 16:29:42 -0500

    finish routes *fixed error in view routes

--- routes/view/index.js
+++ routes/view/index.js
@@ -1,9 +1,23 @@
 let router = require("express").Router();
-let apiRoutes = require("./api");
-let viewRoutes = require("./view");
+let db = require("../../models");
 
-router.use("/api", apiRoutes);
-router.use("/", viewRoutes);
+// route that renders homepage
+router.get("/", function(req, res) {
+    db.Headline.find({ saved: false })
+        .sort({ date: -1 })
+        .then(function(dbArticles) {
+            res.render("home", { articles: dbArticles });
+        });
+});
+
+// This route renders the saved handlebars page
+router.get("/saved", function(req, res) {
+    db.Headline.find({ saved: true })
+        .sort({ date: -1 })
+        .then(function(dbArticles) {
+            res.render("saved", { articles: dbArticles});
+        });
+});
 
 module.exports = router;
 
